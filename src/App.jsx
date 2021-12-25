c0d7404871ca731c3eed2031ddda09c0f2d2a8ae
Harmy1104 <harmy1410@gmail.com>
2021-12-25 17:55:22 +0000

    build job

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.jsx</code> and save to reload!
-				</p>
-				<span className="App-link">Hello from codedamn :)</span>
+				<span className="App-link">CI/CD Github Actions</span>
 			</header>
 		</div>
 	)
